1bf6194ea66204b1faefe0c73b42ca60347b2216
thedruny <thedruny@gmail.com>
2020-01-17 22:15:38 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://www.logaster.ru/static/v3/img/products/logo.png" alt="логотип"></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://bipbap.ru/wp-content/uploads/2017/04/maxresdefault-2-6.jpg" alt="banner" />
+        </div>
+        <div>
+            ava + description
+          <img src="https://avatars.mds.yandex.net/get-pdb/2833210/e9dc7fa6-c6e1-4558-9441-7f3247f68a96/s375" alt="logo" />
+        </div>
+        <div>
+            My post
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+}
+
+export default Profile;
