47c216dbca17500f10d5075c82bc1863eda08388
thedruny <thedruny@gmail.com>
2020-02-28 09:16:26 +0200

    Refactoring api.js

--- src/api/api.js
+++ src/api/api.js
@@ -21,6 +21,13 @@ export const usersAPI = {
             return instance.delete(`follow/${userId}`)
       },
       getProfile(userId) {
+            console.warn('Obsolete method. Please use profileAPI object.')
+            return profileAPI.getProfile(userId)
+      }
+};
+
+export const profileAPI = {
+      getProfile(userId) {
             return instance.get(`profile/${userId}`)
       }
 };
