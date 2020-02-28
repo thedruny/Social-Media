cf5de1bcdced4aa3c111e6d0e574bfe6cee1fd5e
thedruny <thedruny@gmail.com>
2020-02-28 16:49:39 +0200

    Add setStatus. redux-thunk getStatus, updateStatus

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Profile from './Profile';
 import { connect } from 'react-redux';
-import { getUserProfile } from '././../../redux/profile-reducer'
+import { getUserProfile, getStatus, updateStatus } from '././../../redux/profile-reducer'
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 
@@ -13,16 +13,18 @@ class ProfileContainer extends React.Component {
         if (!userId) { userId = 2; }
 
         this.props.getUserProfile(userId);
+        this.props.getStatus(userId)
     }
     render() { return (< Profile {...this.props} profile={this.props.profile} />) }
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    status: state.profilePage.status
 });
 
 export default compose(
-    connect(mapStateToProps, { getUserProfile }),
+    connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
     withRouter,
     // withAuthRedirect
 )(ProfileContainer)
