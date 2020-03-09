eba849a00cde0009bc79ef0e9dac146d3b290013
thedruny <thedruny@gmail.com>
2020-03-09 16:31:59 +0200

    Add React.lazy and refactoring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css'
-import News from './components/News/News';
 import { Route, withRouter } from 'react-router-dom';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+import ProfileContainer from './components/Profile/ProfileContainer'
 import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './components/Login/Login';
 import { initializeApp } from './redux/app-reducer'
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import Preloader from './components/common/Preloader/Preloader';
 import 'semantic-ui-css/semantic.min.css'
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const Login = React.lazy(() => import('./components/Login/Login'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 class App extends Component {
 
@@ -28,13 +26,12 @@ class App extends Component {
       <div className="app-wrapper" >
         <HeaderContainer />
         <div className="app-wrapper-content">
-          <Route path='/dialogs' render={() => <DialogsContainer />} />
+          <Suspense fallback={<Preloader />}>
+            <Route path='/dialogs' render={() => <DialogsContainer />} />
+            <Route path='/login' render={() => <Login />} />
+            <Route path='/users' render={() => <UsersContainer />} />
+          </Suspense>
           <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
-          <Route path='/users' render={() => <UsersContainer />} />
-          <Route path='/news' render={() => <News />} />
-          <Route path='/audios' render={() => <Music />} />
-          <Route path='/settings' render={() => <Settings />} />
-          <Route path='/login' render={() => <Login />} />
         </div>
       </div>
     );
